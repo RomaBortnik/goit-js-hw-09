c2ee01278c8cf5962aa4426f9bf1f8b6beb34f0a
RomaBortnik <bortnik25051998@gmail.com>
2023-02-26 11:45:40 +0200

    1st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
-console.log('hello');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', onButtonStart);
+stopBtn.addEventListener('click', onButtonStop);
+
+stopBtn.setAttribute('disabled', 'true');
+let intervalId = null;
+
+function onButtonStart(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  stopBtn.removeAttribute('disabled');
+  startBtn.setAttribute('disabled', 'true');
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onButtonStop(event) {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'true');
+
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
